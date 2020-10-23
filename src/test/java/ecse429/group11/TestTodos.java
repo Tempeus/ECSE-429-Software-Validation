21c1d1cec62692332168c11861efe8bb1ee2c671
Tempeus <nekugm@gmail.com>
2020-10-23 19:02:34 -0400

    Added more tests

--- src/test/java/ecse429/group11/TestTodos.java
+++ src/test/java/ecse429/group11/TestTodos.java
@@ -4,6 +4,8 @@ import static ecse429.group11.TodoInstance.send;
 import static org.junit.Assert.assertEquals;
 
 
+import io.restassured.internal.mapping.JsonbMapper;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -11,15 +13,15 @@ import java.io.IOException;
 
 public class TestTodos {
 
-
-
-
 //    @Before
 //    public void startInstance(){
 //        TodoInstance.runApplication();
 //    }
 
-
+//    @After
+//    public void deadifyInstance(){
+//        TodoInstance.killInstance();
+//    }
 
     //GET todos
     @Test
@@ -33,6 +35,57 @@ public class TestTodos {
         assertEquals(2,response.getJSONArray("todos").length());
     }
 
+    @Test
+    public void testGetByTitle() throws IOException {
+        String valid_title = "/todos?title=file%20paperwork";
+        JSONObject response = TodoInstance.send("GET",valid_title);
+        String result = response.getJSONArray("todos").getJSONObject(0).getString("title");
+        assertEquals(result,"file paperwork");
+    }
+
+    @Test
+    public void testGetByInvalidTitle() throws IOException {
+        String invalid_title = "/todos?title=mothman";
+        JSONObject response = send("GET", invalid_title);
+        assertEquals(0,response.getJSONArray("todos").length());
+    }
+
+    @Test
+    public void testGetByDoneStatusTrue() throws IOException {
+        String donestatus_true = "/todos?doneStatus=true";
+        JSONObject response = send("GET", donestatus_true);
+        assertEquals(0,response.getJSONArray("todos").length());
+    }
+
+    @Test
+    public void testGetByDoneStatusFalse() throws IOException {
+        String donestatus_true = "/todos?doneStatus=false";
+        JSONObject response = send("GET", donestatus_true);
+        assertEquals(2,response.getJSONArray("todos").length());
+    }
+
+    @Test
+    public void testGetByInvalidDoneStatus() throws IOException {
+        String donestatus_invalid = "/todos?doneStatus=maybe";
+        JSONObject response = send("GET", donestatus_invalid);
+        assertEquals(0,response.getJSONArray("todos").length());
+    }
+
+    @Test
+    public void testGetByDescription() throws IOException {
+        String description = "/todos?description=";
+        JSONObject response = send("GET", description);
+        assertEquals(2,response.getJSONArray("todos").length());
+    }
+
+    @Test
+    public void testGetByInvalidDescription() throws IOException{
+        String description = "/todos?description=NonExistent";
+        JSONObject response = send("GET", description);
+        assertEquals(0,response.getJSONArray("todos").length());
+    }
+
+    //GET todos/id
     @Test
     public void testGetSpecificTodoUsingID() throws IOException{
         JSONObject response = send("GET", "/todos/1");
@@ -53,22 +106,53 @@ public class TestTodos {
         assertEquals(TodoInstance.getStatusCode(valid_request),TodoInstance.SC_SUCCESS);
     }
 
-
-
-
-
     //POST todos
+    @Test
+    public void testCreateValidTodo(){
+        String validID = "/todos";
+    }
 
     @Test
-    public void testCreateTodo(){
+    public void testCreateWithOnlyTitle(){
 
     }
+
+    @Test
+    public void TestCreateWithExistingTitle(){
+
+    }
+
+    @Test
+    public void testCreateInvalidTodo(){
+
+    }
+
+    //POST todos/id
+    @Test
+    public void testOverrideWithID(){
+
+    }
+
+    @Test
+    public void testOverrideTitle(){
+
+    }
+
+    @Test
+    public void testOverrideDoneStatus(){
+
+    }
+
+    @Test
+    public void testOverrideDescription(){
+
+    }
+
     //HEAD todos
 
+    //HEAD todos/id
 
-//    @After
-//    public void deadifyInstance(){
-//        TodoInstance.killInstance();
-//    }
+
+
 
 }
