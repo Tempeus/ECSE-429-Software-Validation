1d9e13d8e6e407549ee54cff8b7622fc6d01f8aa
Steven Luu <luu.steveluu@gmail.com>
2020-10-24 21:16:10 -0400

    completed update/Delete .py

--- updateSampleData.py
+++ updateSampleData.py
@@ -136,7 +136,7 @@ projects = requests.get(url=URL + "/projects")
 print(projects.json())
 
 print("===================PUT /projects/:id=======================")
-# POST /projects/:id
+# PUT /projects/:id
 projects = requests.get(url=URL + "/projects")
 projectsJson = projects.json().get("projects")
 sizeProjects = len(projects.json().get("projects"))
@@ -144,7 +144,7 @@ for i in range(sizeProjects):
     projectsID = projectsJson[i].get("id")
     projectData = {"description": "PUT ammend " +
                    projectsJson[i].get("title") + ". ID#: " + projectsID}
-    r = requests.post(url=URL + "/projects/" + projectsID, json=projectData)
+    r = requests.put(url=URL + "/projects/" + projectsID, json=projectData)
 
 projects = requests.get(url=URL + "/projects")
 
@@ -162,7 +162,7 @@ sizeProjects = len(projects.json().get("projects"))
 for i in range(sizeProjects):
     projectsID = projectsJson[i].get("id")
     try:
-        projectData = {"id": todosData[0].get("id")}
+        projectData = {"id": todosJson[0].get("id")}
         r = requests.post(url=URL + "/projects/" + projectsID +
                           "/tasks", json=projectData)
     except:
@@ -184,10 +184,181 @@ for i in range(sizeProjects):
     projectsID = projectsJson[i].get("id")
     try:
         r = requests.delete(url=URL + "/projects/" + projectsID + "/tasks/" +
-                            requests.get(url=URL + "/projects").json().get("tasks")[0].get("id"))
+                            requests.get(url=URL + "/projects").json().get("projects")[i].get("tasks")[0].get("id"))
     except:
         print("PROJECTS " + projectsID + " has no tasks")
 
 projects = requests.get(url=URL + "/projects")
 print(projects.json())
 
+print("====================POST /projects/:id/categories======================")
+
+# POST /projects/:id/tasks
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+
+projects = requests.get(url=URL + "/projects")
+projectsJson = projects.json().get("projects")
+sizeProjects = len(projects.json().get("projects"))
+for i in range(sizeProjects):
+    projectsID = projectsJson[i].get("id")
+    try:
+        projectData = {"id": categoriesJson[0].get("id")}
+        r = requests.post(url=URL + "/projects/" + projectsID +
+                          "/categories", json=projectData)
+    except:
+        print("NO CATEGORIES")
+
+projects = requests.get(url=URL + "/projects")
+print(projects.json())
+
+print("====================DELETE /projects/:id/categories/:id======================")
+
+# DELETE /todos/:id/categories/:id
+# if has category DELETE first category of task
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+projects = requests.get(url=URL + "/projects")
+projectsJson = projects.json().get("projects")
+sizeProjects = len(projects.json().get("projects"))
+for i in range(sizeProjects):
+    projectsID = projectsJson[i].get("id")
+    try:
+        r = requests.delete(url=URL + "/projects/" + projectsID + "/categories/" +
+                            requests.get(url=URL + "/projects").json().get("projects")[i].get("categories")[0].get("id"))
+    except:
+        print("PROJECTS " + projectsID + " has no categories")
+
+projects = requests.get(url=URL + "/projects")
+print(projects.json())
+
+print("===================PUT /categories/:id=======================")
+# POST /projects/:id
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+sizeCategories = len(categories.json().get("categories"))
+for i in range(sizeCategories):
+    categoriesID = categoriesJson[i].get("id")
+    categoryData = {"description": "PUT ammend " +
+                   categoriesJson[i].get("title") + ". ID#: " + categoriesID}
+    r = requests.put(url=URL + "/categories/" + categoriesID, json=categoryData)
+
+categories = requests.get(url=URL + "/categories")
+
+print(categories.json())
+
+print("===================PUT /categories/:id=======================")
+# PUT /categories/:id
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+sizeCategories = len(categories.json().get("categories"))
+for i in range(sizeCategories):
+    categoriesID = categoriesJson[i].get("id")
+    categoryData = {"description": "PUT ammend " +
+                   categoriesJson[i].get("title") + ". ID#: " + categoriesID}
+    r = requests.put(url=URL + "/categories/" + categoriesID, json=categoryData)
+
+categories = requests.get(url=URL + "/categories")
+
+print(categories.json())
+
+print("===================POST /categories/:id=======================")
+# POST /categories/:id
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+sizeCategories = len(categories.json().get("categories"))
+for i in range(sizeCategories):
+    categoriesID = categoriesJson[i].get("id")
+    categoryData = {"description": "POST ammend " +
+                   categoriesJson[i].get("title") + ". ID#: " + categoriesID}
+    r = requests.post(url=URL + "/categories/" + categoriesID, json=categoryData)
+
+categories = requests.get(url=URL + "/categories")
+
+print(categories.json())
+
+print("====================POST /categories/:id/todos======================")
+
+# POST /categories/:id/todos
+todos = requests.get(url=URL + "/todos")
+todosJson = todos.json().get("todos")
+
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+sizeCategories = len(categories.json().get("categories"))
+for i in range(sizeCategories):
+    categoriesID = categoriesJson[i].get("id")
+    try:
+        categoryData = {"id": todosJson[0].get("id")}
+        r = requests.post(url=URL + "/categories/" + categoriesID +
+                          "/todos", json=projectData)
+    except:
+        print("NO TODOS")
+
+categories = requests.get(url=URL + "/categories")
+print(categories.json())
+
+print("====================DELETE /categories/:id/todos/:id======================")
+
+# DELETE /categories/:id/todos/:id
+# if has category DELETE first category of task
+todos = requests.get(url=URL + "/todos")
+todosJson = todos.json().get("todos")
+sizeTodos = len(todos.json().get("todos"))
+
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+sizeCategories = len(categories.json().get("categories"))
+for i in range(sizeTodos):
+    try:
+        categoriesID = requests.get(url=URL + "/categories").json().get("categories")[0].get("id")
+        r = requests.delete(url=URL + "/categories/" + categoriesID + "/todos/" +
+                            requests.get(url=URL + "/todos").json().get("todos")[i].get("id"))
+    except:
+        print("CATEGORY " + categoriesID + " has no todos")
+
+categories = requests.get(url=URL + "/categories")
+print(categories.json())
+
+print("====================POST /categories/:id/projects======================")
+
+# POST /categories/:id/projects
+projects = requests.get(url=URL + "/projects")
+projectsJson = projects.json().get("projects")
+
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+sizeCategories = len(categories.json().get("categories"))
+for i in range(sizeCategories):
+    categoriesID = categoriesJson[i].get("id")
+    try:
+        categoryData = {"id": projectsJson[0].get("id")}
+        r = requests.post(url=URL + "/categories/" + categoriesID +
+                          "/projects", json=projectData)
+    except:
+        print("NO PROJECTS")
+
+categories = requests.get(url=URL + "/categories")
+print(categories.json())
+
+print("====================DELETE /categories/:id/projects/:id======================")
+
+# DELETE /categories/:id/projects/:id
+# if has category DELETE first category of task
+projects = requests.get(url=URL + "/projects")
+projectsJson = projects.json().get("projects")
+sizeProjects = len(projects.json().get("projects"))
+
+categories = requests.get(url=URL + "/categories")
+categoriesJson = categories.json().get("categories")
+sizeCategories = len(categories.json().get("categories"))
+for i in range(sizeProjects):
+    try:
+        categoriesID = requests.get(url=URL + "/categories").json().get("categories")[0].get("id")
+        r = requests.delete(url=URL + "/categories/" + categoriesID + "/projects/" +
+                            requests.get(url=URL + "/projects").json().get("projects")[i].get("id"))
+    except:
+        print("CATEGORY " + categoriesID + " has no projects")
+
+categories = requests.get(url=URL + "/categories")
+print(categories.json())
