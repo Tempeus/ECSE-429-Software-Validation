4db9dbf6f05cb577acd47e938e0a7eb642ae795a
Tempeus <nekugm@gmail.com>
2020-11-14 16:38:16 -0500

    Added Cucumber runner and dependencies

---
+++ src/test/ecse429/group11/cucumber/CucumberTestRunner.java
@@ -0,0 +1,12 @@
+package ecse429.group11.cucumber;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "pretty")
+public class CucumberTestRunner {
+}
